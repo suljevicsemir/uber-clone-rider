92d91b049488fcfef6e7a4cf6f7d1291d45ae4a3
semirsuljevic <suljevic.semir@gmail.com>
2021-02-03 10:37:46 +0100

    Project and Firebase setup.

---
+++ uber_clone/lib/mock_home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeMock extends StatefulWidget {
+  @override
+  _HomeMockState createState() => _HomeMockState();
+}
+
+class _HomeMockState extends State<HomeMock> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Mock Home'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
