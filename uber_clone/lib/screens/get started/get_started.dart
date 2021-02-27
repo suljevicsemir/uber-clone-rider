8d21734dd07cb0d87dce772e41b794db380c2476
semirsuljevic <suljevic.semir@gmail.com>
2021-02-28 00:51:35 +0100

    GetStarted screen transformed into StatelessWidget Replaced Spacers with Expanded widgets for better responsive design Bigger 'Move with Safety' title, set to maximum across the screen
    Word file added to keep track of issues in development

--- uber_clone/lib/screens/get started/get_started.dart
+++ uber_clone/lib/screens/get started/get_started.dart
@@ -1,26 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
-import 'package:uber_clone/services/secure_storage.dart';
 
-class GetStarted extends StatefulWidget {
+class GetStarted extends StatelessWidget {
 
   static const String route = '/getStarted';
-
-  @override
-  _GetStartedState createState() => _GetStartedState();
-}
-
-class _GetStartedState extends State<GetStarted> {
-
-  SecureStorage provider = SecureStorage();
-
-
-  @override
-  void initState()  {
-    super.initState();
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,33 +19,43 @@ class _GetStartedState extends State<GetStarted> {
             padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
             color: const Color(0xff286ef0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.05),
-                      child: Text('Uber', style: TextStyle(color: Colors.white, fontSize: 30))
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                        alignment: Alignment.center,
+                        child: Text('Uber', style: TextStyle(color: Colors.white, fontSize: 42, letterSpacing: 0.8))
+                    ),
                   ),
-                  Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.07),
-                      child: Image.asset('assets/images/driver_rider_mask.png', fit: BoxFit.cover,)
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                        alignment: Alignment.topCenter,
+                        child: Image.asset('assets/images/driver_rider_mask.png')
+                    ),
                   ),
                   //Spacer(),
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
-                    child: Text('Move with Safety', style: TextStyle(color: Colors.white, fontSize: 30)),
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text('Move with Safety', style: TextStyle(color: Colors.white, fontSize: 80))
+                      ),
+                    ),
                   ),
-                  Spacer(),
                   ElevatedButton(
-                    onPressed: () =>
-                        Navigator.pushNamed(context, '/pickLoginType'),
+                    onPressed: () async => await Navigator.pushNamed(context, '/pickLoginType'),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Spacer(),
-                        Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 22),),
-                        Spacer(),
+                        Expanded(
+                            child: Center(
+                                child: Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 22))
+                            )
+                        ),
                         Container(
                             margin: EdgeInsets.only(right: 8),
                             child: Icon(Icons.arrow_right_alt_outlined, size: 36,)
