b642d4c99835d44361eb1027803027e48da2db14
semirsuljevic <suljevic.semir@gmail.com>
2021-02-28 01:37:24 +0100

    ChooseAccount changes, bigger fons and FontAwesome icons replaced with PNG images, transformed the whole screen into StatelessWidget.

--- uber_clone/lib/screens/get started/choose_account.dart
+++ uber_clone/lib/screens/get started/choose_account.dart
@@ -1,88 +1,82 @@
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:uber_clone/components/authentication_wrapper.dart';
 import 'package:uber_clone/models/user_data.dart';
 import 'package:uber_clone/services/authentication_service.dart';
 
-class ChooseAccount extends StatefulWidget {
+class ChooseAccount extends StatelessWidget {
 
   static const String route = '/chooseAccount';
 
-
-  @override
-  _ChooseAccountState createState() => _ChooseAccountState();
-}
-
-class _ChooseAccountState extends State<ChooseAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.only(left: 10, right: 10, bottom: 5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BackButton(),
-              Container(
-                margin: EdgeInsets.only(left: 10, top: MediaQuery.of(context).size.height * 0.05),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Choose an account'),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                    Material(
-                      child: InkWell(
-                        splashColor: Colors.grey,
-                        onTap: () async {
-                          final UserData result = await Provider.of<AuthenticationService>(context, listen: false).signInWithFacebook();
-                          if(result != null) {
-                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AuthenticationWrapper()), (_) => false);
-                          }
-                        },
-                        child: Container(
-                          padding: EdgeInsets.only(top: 5, bottom: 5),
-                          child: Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.facebookSquare, color: const Color(0xff4267B2),),
-                              SizedBox(width: 0.05 * MediaQuery.of(context).size.width,),
-                              Text('Facebook')
-                            ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(left: 10, right: 10, bottom: 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BackButton(),
+                Container(
+                  margin: EdgeInsets.only(left: 10, top: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      FittedBox(child: Text('Choose an account', style: TextStyle(fontSize: 22),)),
+                      SizedBox(height: 30),
+                      Material(
+                        child: InkWell(
+                          splashColor: Colors.grey,
+                          onTap: () async {
+                            final UserData result = await Provider.of<AuthenticationService>(context, listen: false).signInWithFacebook();
+                            if(result != null) {
+                              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AuthenticationWrapper()), (_) => false);
+                            }
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(top: 5, bottom: 5),
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/facebook_icon.png', scale: 16,),
+                                SizedBox(width: 20),
+                                Text('Facebook', style: TextStyle(fontSize: 18),)
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                    Material(
-                      child: InkWell(
-                        splashColor: Colors.grey,
-                        onTap: ()  async{
-                          final UserData result = await Provider.of<AuthenticationService>(context, listen: false).signInWithGoogle();
-                          if( result != null) {
-                            Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AuthenticationWrapper()), (_) => false);
-                          }
-                        },
-                        child: Container(
-                          padding: EdgeInsets.only(top: 5, bottom: 5),
-                          child: Row(
-                            children: [
-                              FaIcon(FontAwesomeIcons.google, color: Colors.blue,),
-                              SizedBox(width: 0.05 * MediaQuery.of(context).size.width,),
-                              Text('Google')
-                            ],
+                      SizedBox(height: 30),
+                      Material(
+                        child: InkWell(
+                          splashColor: Colors.grey,
+                          onTap: ()  async{
+                            final UserData result = await Provider.of<AuthenticationService>(context, listen: false).signInWithGoogle();
+                            if( result != null) {
+                              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AuthenticationWrapper()), (_) => false);
+                            }
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(top: 5, bottom: 5),
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/google_icon.png', scale: 16,),
+                                SizedBox(width: 20),
+                                Text('Google', style: TextStyle(fontSize: 18))
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    Text('By clicking on a social option you may receive an SMS for verification. Message and data rates may apply'),
-                  ],
+                      SizedBox(height: 20,),
+                      Text('By clicking on a social option you may receive an SMS for verification. Message and data rates may apply', style: TextStyle(fontSize: 18),),
+                    ],
+                  ),
                 ),
-              ),
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
